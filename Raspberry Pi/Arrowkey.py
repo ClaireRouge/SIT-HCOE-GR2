1ccb46bd2dbbc87600479fa200d482d816a7e5af
David Lolck <david@lolck.com>
2017-09-27 19:00:19 +0200

    linux??? support

--- Raspberry Pi/Arrowkey.py
+++ Raspberry Pi/Arrowkey.py
@@ -1,6 +1,31 @@
 import pygame,sys
 #import serial
 #ser = serial.Serial('/dev/ttyACM0', 9600)
+import sys,tty,termios
+class _Getch:
+    def __call__(self):
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setraw(sys.stdin.fileno())
+                ch = sys.stdin.read(3)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            return ch
+
+def getKey():
+        inkey = _Getch()
+        while(1):
+                k=inkey()
+                if k!='':break
+        if k=='\x1b[A':
+                return "up"
+        elif k=='\x1b[B':
+                return "down"
+        elif k=='\x1b[C':
+                return "right"
+        elif k=='\x1b[D':
+                return "left"
 l = (150,150)
 d = (150,50)
 window = pygame.display.set_mode((10,10))
@@ -11,17 +36,17 @@ while True:
 			sys.exit()
 	l = (150,150)
 	keys = pygame.key.get_pressed()
-
-	if keys[pygame.K_LEFT]:
+	k = get()
+	if k == "up":
 		l = (150,50)
 
-	if keys[pygame.K_RIGHT]:
+	if k == "down":
 		l = (50,150)
 
-	if keys[pygame.K_UP]:
+	if k == "left":
 		print str(l[0]) + " " + str(l[1])
 		#ser.write(str(l[0]) + " " + str(l[1]))
 
-	if keys[pygame.K_DOWN]:
+	if k == "right":
 		print str(-l[1]) + " " + str(-l[0])
 		#ser.write(str(-l[1]) + " " + str(-l[0]))
