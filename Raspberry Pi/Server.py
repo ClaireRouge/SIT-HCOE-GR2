c6f6096112010a632622351076c5ed736f24e7c1
MnomisC <scdockweiler@gmail.com>
2017-10-16 14:20:36 +0200

    More problems

--- Raspberry Pi/Server.py
+++ Raspberry Pi/Server.py
@@ -4,7 +4,7 @@ import socket
 
 TCP_IP = '192.168.0.22'    # Static ref file???
 TCP_PORT = 313         # Static ref file???
-BUFFER_SIZE = 1024       # Static ref file???
+BUFFER_SIZE = 256       # Static ref file???
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
@@ -12,6 +12,7 @@ while True:
 	s.listen(1)
 	conn, addr = s.accept()
 	data = conn.recv(BUFFER_SIZE)
+	if not data: break
 	print data
 	#conn.send(data)
 conn.close()
