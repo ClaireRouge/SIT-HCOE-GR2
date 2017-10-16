d4b9134c75416d495984c9f0f6a737fb599c65d7
MnomisC <scdockweiler@gmail.com>
2017-10-16 14:22:53 +0200

    Tests

--- Raspberry Pi/Server.py
+++ Raspberry Pi/Server.py
@@ -9,9 +9,12 @@ BUFFER_SIZE = 256       # Static ref file???
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 while True:
+	print "Kage1"
 	s.listen(1)
+	print "Kage2"
 	conn, addr = s.accept()
 	data = conn.recv(BUFFER_SIZE)
+	print "Kage3"
 	if not data: break
 	print data
 	#conn.send(data)
