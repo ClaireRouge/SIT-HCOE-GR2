1e975f0da42cd3b29f2d35490b4eb652aeb8da60
Derzeed <Derzeedk@gmail.com>
2017-10-20 17:13:36 +0200

    Now drives backwards when in corner.

--- Raspberry Pi/FearThePoints.py
+++ Raspberry Pi/FearThePoints.py
@@ -1,5 +1,13 @@
 import math
 import numpy
+import pygame
+
+corner_counter = 0
+CORNER_MAX = 10
+MAX_DIR = math.pi/3
+PRE_TANH_COEFF = 0.25
+CORNER_WEIGHT = 0.0020
+START_DIRECTION = -1
 
 def is_sorted(target,sortnum):
     for index,value in enumerate(target):
@@ -29,7 +37,7 @@ class Point(object):
         #print angle
         #creating a weight. Can be changed later
 
-        self.weight = self.angle*math.cos(self.angle)*self.angle_dist * 10000/self.length**2
+        self.weight = self.angle*math.cos(self.angle)*self.angle_dist * 1/self.length**2
         #print self.angle,self.length,self.weight
         #print self.weight, self.angle
 
@@ -38,6 +46,7 @@ def run(point_data):
     """
     point_data: list of touples containing (length,angle)
     """
+    global corner_counter
     if not is_sorted(point_data,1):
         sorted(point_data,key = lambda x:x[1])
     #They are now sorted
@@ -61,12 +70,23 @@ def run(point_data):
     nom = (abs(left),abs(right))[abs(left)<abs(right)]
     #print left,right,nom
     denom = (left,right)[abs(left)>abs(right)]
-    direction = (nom/denom) * 0.25
-    #print direction
-    #print direction, "1"
+    direction = (nom/denom) * PRE_TANH_COEFF
+    #print left,right,direction
+    if left > CORNER_WEIGHT and right < -CORNER_WEIGHT and direction < 2*PRE_TANH_COEFF and corner_counter == 0: #determined by testing
+        corner_counter = CORNER_MAX
+        pygame.time.wait(2000)
+        #print corner_counter
+    if corner_counter != 0:
+        #its in a cornor
+        if CORNER_MAX-corner_counter < CORNER_MAX/2:
+            direction = math.pi
+        else:
+            direction = (math.pi- math.pi/(CORNER_MAX))*START_DIRECTION
+        corner_counter -= 1
 
-    direction = math.tanh(direction) * math.pi/3
-    #print direction, "2"
+    else:
+        direction = math.tanh(direction) * MAX_DIR
+    #print direction
     return direction
 if __name__ == '__main__':
     run([(1,-90),(1,-30)])
